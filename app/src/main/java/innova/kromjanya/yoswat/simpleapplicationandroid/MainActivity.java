61f6eec1eb46f8cd4bef6ef67f922775d133b6ce
YOTYOI <yoswat.photodesign@gmail.com>
2017-10-10 14:24:44 +0700

    Add Fragment success

--- app/src/main/java/innova/kromjanya/yoswat/simpleapplicationandroid/MainActivity.java
+++ app/src/main/java/innova/kromjanya/yoswat/simpleapplicationandroid/MainActivity.java
@@ -3,12 +3,21 @@ package innova.kromjanya.yoswat.simpleapplicationandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import innova.kromjanya.yoswat.simpleapplicationandroid.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null){
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.contentFragmentmain, new MainFragment()).commit();
+        }
+
     } // main method
 
 }// main class
